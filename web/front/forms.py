47fbf68db4b31b1da039a15487c7327f670aa9a4
Brian Neeland <brian.neeland@protonmail.com>
2021-10-30 05:59:23 -0600

    Printing new user email in custom signup form

--- web/front/forms.py
+++ web/front/forms.py
@@ -2,6 +2,13 @@ from django import forms
 from allauth.account.forms import SignupForm
 
 class APISignupForm(SignupForm):
+    def save(self, request):
+        user = super(APISignupForm, self).save(request)
+        user.email = self.cleaned_data['email']
+        print(user.email, flush=True)
+        user.save()
+        return user
+
     class Meta:
         # model = get_user_model()
         fields = ('email', 'password1', 'password2')
